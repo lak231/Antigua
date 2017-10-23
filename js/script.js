3b3809588c02fe38839c8255060635c55f5959fa
Khoi Le <kal037@bucknell.edu>
2017-10-22 23:54:07 -0400

    done

--- js/script.js
+++ js/script.js
@@ -29,3 +29,37 @@ $('.card').bind('click', function() {
     $('.card').removeClass('active');
     $('.marker').removeClass('inactive');
 });
+
+function show_full_info() {
+    console.log("eyyy");
+    var modal = document.getElementsByClassName("modal")[0];
+
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+    modal.style.display = "block";
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    };
+}
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
